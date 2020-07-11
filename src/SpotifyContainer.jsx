be9ed2eb7c96d5a5704f2dc406fb8872dbb5ea3f
KatieJessupMcD <katie.jessupmcd@gmail.com>
2020-07-10 20:23:04 -0700

    first commit

---
+++ src/SpotifyContainer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import './SpotifyContainer.css';
+
+class SpotifyContainer extends Component {
+  static defaultProps = {};
+  render() {
+    return <div className='SpotifyContainer'>
+      HELLO
+      </div>;
+  }
+}
+
+export default SpotifyContainer;
