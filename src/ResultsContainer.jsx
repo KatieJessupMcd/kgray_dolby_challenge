9f9f04867e4d104f7b7f71002c207d9f8e58bae2
KatieJessupMcD <katie.jessupmcd@gmail.com>
2020-07-13 12:22:19 -0700

    impleted audio playing when you click on queried album image; there is a rare-ish edge case from the api call, some tracks do not provide a preview url, will punt on this for now and make a nice user exp in that case if I have time

--- src/ResultsContainer.jsx
+++ src/ResultsContainer.jsx
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
 
 class ResultsContainer extends Component {
-  static defaultProps = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      play: false,
+    };
+    this.audio = new Audio(this.props.albumData.trackPreviewUrl);
+    this.playAudio = this.playAudio.bind(this);
+  }
+
+  playAudio() {
+    const audioEl = document.getElementsByClassName("audio-element")[0]
+    audioEl.load()
+    audioEl.play()
+  }
+
   render() {
-    if (this.props.displayAlbum){
+    if (this.props.displayAlbum) {
       return (
-        <div className="ResultsContainer" id="album-cover">
-        Album Name: {this.props.albumData.album.name}
-        Album Image: <div><img src={this.props.albumData.album.images[0].url} alt="albumImage" className="img-responsive"/></div>
-        Album Preview URL: {this.props.albumData.trackPreviewUrl}
-      </div> 
-      )
+        <div>
+          <div
+            className="ResultsContainer"
+            id="album-cover"
+            onClick={this.playAudio}
+          >
+            Album Name: {this.props.albumData.album.name}
+            Album Image:{' '}
+            <div>
+              <img
+                src={this.props.albumData.album.images[0].url}
+                alt="albumImage"
+                className="img-responsive"
+              />
+            </div>
+            Album Preview URL: {this.props.albumData.trackPreviewUrl}
+          </div>
+          <div>
+            <audio className="audio-element">
+              <source src={this.props.albumData.trackPreviewUrl}></source>
+            </audio>
+          </div>
+        </div>
+      );
     } else {
-      return (
-        <div></div>
-      )
+      return <div></div>;
     }
   }
 }
