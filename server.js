0d73adb30b88d7848608e3070a9d927d6a7eae49
KatieJessupMcD <katie.jessupmcd@gmail.com>
2020-07-16 05:44:26 -0700

    file path for heroku

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('client/build'));
+  app.use(express.static('public'));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
   });
 }
 
